3af98b4972b9c127d02feb5533a817f0e0b071bb
Zacahry Walters <zachary0191@gmail.com>
2012-11-22 19:48:04 -0500

    Initial Commit

---
+++ Assignment 6/Firm/src/firm/Commission.java
@@ -0,0 +1,14 @@
+package firm;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zachary Walters
+ */
+public class Commission {
+    
+}
---
+++ Assignment 6/Firm/src/firm/Firm.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firm;
+
+/**
+ *
+ * @author Zachary Walters
+ */
+public class Firm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
