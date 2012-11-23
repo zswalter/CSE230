df2372894fd6c7c186391e42dba66f5939a0adff
zwalters <zwalters@Zach16.default>
2012-11-23 00:09:09 -0500

    Test of Linux Commit

--- Assignment 6/Firm/src/firm/Commission.java
+++ Assignment 6/Firm/src/firm/Commission.java
@@ -1,14 +1,5 @@
 package firm;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Zachary Walters
- */
 public class Commission {
     
 }
